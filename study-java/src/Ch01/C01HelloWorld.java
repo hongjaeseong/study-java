4aa6dd24d13271233fd370382b607cc3e98f86cc
hongsaeroy <hjs971102@naver.com>
2024-05-16 23:28:04 +0900

    study java basic

--- study-java/src/Ch01/C01HelloWorld.java
+++ study-java/src/Ch01/C01HelloWorld.java
@@ -7,14 +7,11 @@ public class C01HelloWorld // 클래스 영역(객체지향 문법)
 
 	public static void main(String[] args)	//메서드 영역(절차지향 문법) 
 	{
+		System.out.println("Hello World!");
 		//메서드 종류 
 		//라이브러리 메서드	 : 미리 제공되어지는 메서드
 		//사용자정의 메서드  : 개발자에의해 만들어지는 메서드
 		//main 메서드		 : 최초 실행 메서드
-		
-		
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
 	}
 
 }
