f1b5e28cd3dee0db072998b08b7b369f6021e23a
hongsaeroy <hjs971102@naver.com>
2024-05-17 09:27:40 +0900

    study java System(표준 입출력 클래스)

--- study-java/src/Ch01/C02SystemOut.java
+++ study-java/src/Ch01/C02SystemOut.java
@@ -4,10 +4,6 @@ public class C02SystemOut {
 
 	public static void main(String[] args) {
 
-		// 단축키
-		// 복사 : ctrl + alt + ↓
-		// 주석 : ctrl + /
-
 		// System : OS
 		// System.out : OS가 관리하는 표준 출력 스트림
 
@@ -16,10 +12,10 @@ public class C02SystemOut {
 		// \n : 개행,줄바꿈
 		// \b : 백스페이스
 		// \t : 탭길이(기본 8칸) 만큼 커서이동
-//		System.out.print("HELLO WORLD\n");
-//		System.out.print("HELLO WORLD\n");
-//		System.out.print("HELLO WORLD\n");
-//		System.out.print("HE\tLLOW\tORLD\n");
+		System.out.print("HELLO WORLD\n");		// HELLO WORLD
+		System.out.print("HELLO WORLD\n");		// HELLO WORLD
+		System.out.print("HELLO WORLD\n");		// HELLO WORLD
+		System.out.print("HE\tLLOW\tORLD\n");	// HE	LLOW	ORLD
 
 		// System.out.printf()
 		// format : 형식,서식
@@ -27,19 +23,18 @@ public class C02SystemOut {
 		// %f : 10진수 실수 서식문자
 		// %c : 한문자 서식문자
 		// %s : 문자열 서식문자
-		System.out.printf("%d %d %d \n", 10, 20, 30);
-		System.out.printf("%f %f %f \n", 10.1, 20.1, 30.1);
-		System.out.printf("%c %c %c \n", 'A', 'B', 'C');
-		System.out.printf("%s %s %s \n", "This is", " String ", "Test");
-		System.out.printf("%d.%s : %d\n", 1, "국어", 100);
-//		
-//		
+		System.out.printf("%d %d %d \n", 10, 20, 30);		// 10 20 30 
+		System.out.printf("%f %f %f \n", 10.1, 20.1, 30.1);	// 10.100000 20.100000 30.100000 
+		System.out.printf("%c %c %c \n", 'A', 'B', 'C');	// A B C 
+		System.out.printf("%s %s %s \n", "This is", " String ", "Test");	// This is  String  Test 
+		System.out.printf("%d.%s : %d\n", 1, "국어", 100);	// 1.국어 : 100
+
+		
 		// System.out.println()
-//		System.out.println("HELLO WORLD");
-//		System.out.println("HELLO WORLD");
-//		System.out.println("HELLO WORLD");
-//		
-//		System.out.println("I Say \"HELLO\"");
+		System.out.println("HELLO WORLD");		// HELLO WORLD
+		System.out.println("HELLO WORLD");		// HELLO WORLD
+		System.out.println("HELLO WORLD");		// HELLO WORLD
+		System.out.println("I Say \"HELLO\"");	// I Say "HELLO"
 
 	}
 
